fc1296901339bfa97f11aa5bd1bf294c899d6972
DELL-PRECISION-\Gabriel Opolski <gabriel.opolski98362@gmail.com>
2019-04-18 21:19:01 +0200

    Make frame of Network
    Add: initEdges, giveEntry, countSum, countSigValue, checkToTrain
    functions

--- Network.js
+++ Network.js
@@ -1,23 +1,59 @@
 class Network {
 
    constructor() {
-      this.body = {
-         input: [],
-         edges1: [[], [], [], []],
-         firstHidden: [],
-         edges2: [[], [], [], []],
-         secondHidden: [],
-         edges3: [[], [], []],
-         thirdHidden: [],
-         edges4: [[], []],
-         fourthHidden: [],
-         edges5: [],
-         result: [],
-      };
+      this.sum = 0;
+      this.sigValue = 0;
+      this.input = new Array(15);
+      this.edges = new Array(15);
+      this.result = 0;
    }
 
-   giveEntry(input) {  //array
-      this.body.input = input;
+   initEdges() {
+      for(let i = 0; i < 15; i++) {
+         this.edges[i] = Number(Math.random().toPrecision(2));
+      }
+   }
+
+   giveEntry(input, expectedResult) {
+      if(input.length === 15) {
+         this.input = [...input];
+         this.result = expectedResult;
+      } else {
+         throw Error(`Input has to have 15 values. You give ${input.length}`);
+      }
+   }
+
+   countSum() {
+      let sum = 0;
+      for(let i = 0; i < 15; i++) {
+         sum += (this.input[i] * this.edges[i]);
+      }
+      this.sum = sum;
+   }
+
+   countSigValue() {
+      this.sigValue = Math.exp(this.sum) / (Math.exp(this.sum) + 1);
+      return this.sigValue;
+   }
+
+   checkToTrain() {
+      if(this.result === 0) {
+         if(this.sigValue > 0 && this.sigValue < 0.5) {
+            //activate neuron
+         } else {
+            //change wages
+         }
+      } else {
+         if(this.sigValue >= 0.5 && this.sigValue <= 1) {
+            //activate neuron
+         } else {
+            //change wages
+         }
+      }
+   }
+
+   changeWages() {
+
    }
 
 }
--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const Network = require('./Network');
 
-
-
-
 const net = new Network();
 
-net.giveEntry([1,2,3,4,5,6,7,8]);
+net.giveEntry([1, 2, 3, 3, 3, 4, 4, 5, 4, 4, 4, 3, 3, 4, 5],1);
+net.initEdges();
+net.countSum();
+console.log(net);
+console.log(net.sum);
+console.log(net.countSigValue());
+
